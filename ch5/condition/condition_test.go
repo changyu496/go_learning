2fcc95cb37829a0a94cc8fe2bee713ccfd90a4ec
changyu496 <changyu496@gmail.com>
2019-03-19 00:02:22 +0800

    [feature]增加了switch的用法

--- ch5/condition/condition_test.go
+++ ch5/condition/condition_test.go
@@ -10,3 +10,33 @@ func TestMultiSec(t *testing.T) {
 		t.Log("No!!!")
 	}
 }
+
+/*Go语言的switch可以支持多个值*/
+func TestSwitchMultiSec(t *testing.T) {
+	a := 1
+	for a < 5 {
+		switch a {
+		case 0, 2:
+			t.Log("偶数")
+		case 1, 3:
+			t.Log("奇数")
+		default:
+			t.Log("不属于0到3")
+		}
+		a++
+	}
+}
+
+/*Go语言的switch/case相当于if/else的用法*/
+func TestSwitchCaseMultiSec(t *testing.T) {
+	for a := 0; a < 5; a++ {
+		switch {
+		case a%2 == 0:
+			t.Log("偶数")
+		case a%2 == 1:
+			t.Log("奇数")
+		default:
+			t.Log("奇怪的数")
+		}
+	}
+}
